e4ddcdd771f44d21ac7e1137f50d4cc964ac9bd5
kimjinwook1 <rlawlsdnr628@naver.com>
2022-03-09 14:39:25 +0900

    DB 연동 인증 처리-CustomUserDetailsService

--- src/main/java/hello/springsecurity/repository/UserRepository.java
+++ src/main/java/hello/springsecurity/repository/UserRepository.java
@@ -4,4 +4,5 @@ import hello.springsecurity.domain.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<Account, Long> {
+	Account findByUsername(String username);
 }
--- src/main/java/hello/springsecurity/security/config/SecurityConfig.java
+++ src/main/java/hello/springsecurity/security/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package hello.springsecurity.security.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,21 +9,24 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	private final UserDetailsService userDetailsService;
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		String password = passwordEncoder().encode("1111");
+//		auth.inMemoryAuthentication().withUser("user").password(password).roles("USER"); -> 메모리 인증 방식
+//		auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER","USER");
+//		auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN","MANAGER","USER");
 
-		String password = passwordEncoder().encode("1111");
-
-		auth.inMemoryAuthentication().withUser("user").password(password).roles("USER");
-		auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER","USER");
-		auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN","MANAGER","USER");
+		auth.userDetailsService(userDetailsService);
 	}
 
 	@Bean
