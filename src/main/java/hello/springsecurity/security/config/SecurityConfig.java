e8f3054fbdc79a93cba0910b14c80dc08ec867fa
kimjinwook1 <rlawlsdnr628@naver.com>
2022-03-09 13:31:49 +0900

    정적 자원 관리-WebIgnore 설정

--- src/main/java/hello/springsecurity/security/config/SecurityConfig.java
+++ src/main/java/hello/springsecurity/security/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package hello.springsecurity.security.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -19,8 +21,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		String password = passwordEncoder().encode("1111");
 
 		auth.inMemoryAuthentication().withUser("user").password(password).roles("USER");
-		auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER");
-		auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN");
+		auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER","USER");
+		auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN","MANAGER","USER");
 	}
 
 	@Bean
@@ -29,9 +31,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 
 	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+		//정적 파일들이 보안필터를 거치지 않고 통과처리
+	}
+
+	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
+			.antMatchers("/").permitAll()
 			.antMatchers("/mypage").hasRole("USER")
 			.antMatchers("/messages").hasRole("MANAGER")
 			.antMatchers("/config").hasRole("ADMIN")
