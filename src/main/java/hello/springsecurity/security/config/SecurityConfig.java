3ade0ede8fd2218613ef23ce20f75763aae385d2
kimjinwook1 <rlawlsdnr628@naver.com>
2022-03-09 16:03:29 +0900

    로그아웃 및 인증에 따른 화면 보안 처리

--- src/main/java/hello/springsecurity/security/config/SecurityConfig.java
+++ src/main/java/hello/springsecurity/security/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.and()
 			.formLogin()
 			.loginPage("/login")
-			.loginProcessingUrl("login_proc")
+			.loginProcessingUrl("/login_proc")
 			.defaultSuccessUrl("/")
 			.permitAll();
 	}
